213a45b406748836ab9a8de1426b4671509ccc7c
amani <amanikashema000@gmail.com>
2021-06-04 16:26:16 +0200

    updated information

---
+++ dom.js
@@ -0,0 +1,18 @@
+// Examine The Document
+// console.dir(document)
+
+// // console.log(document.URL);
+// // console.log(document.title);
+// // console.log(document.doctype)
+// // console.log(document.head)
+// // console.log(document.all)
+// // console.log(document.all[10])
+// // console.log(document.forms)
+// // console.log(document.links)
+
+
+// Get element by Id
+
+let itemHeader = document.getElementById("header-title");
+
+console.log(itemHeader)
